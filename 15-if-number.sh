a45aab14c52531160b844e24e6015d0010ffbb3a
moiz <syedmuhammadmoizkazmi151@gmail.com>
2026-04-10 20:43:20 +0500

    added if-number script

---
+++ 15-if-number.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+number=20
+
+if [ $number -eq 5 ]
+then
+   echo "number is eq to 5"
+fi   
+
+if [ $number -lt 15 ]
+then
+   echo "number is less than 15"
+fi 
+
+if [ $number -gt 10 ]
+then
+   echo "number is greater than to 10"
+fi 
